ffaeb5674b039faf80effd8f14956c1ec2a67945
Anthony Thomasson <athomasson.93@gmail.com>
2022-08-01 13:21:59 -0600

    - get file explorer buttons working

--- frontend/src/components/side-bar/FileExplorer.tsx
+++ frontend/src/components/side-bar/FileExplorer.tsx
@@ -1,19 +1,20 @@
+import './../../styles/FileExplorer.css';
 
 function FileExplorer() {
   return (
     <div className="file-explorer">
-      <div className="section section-open">
-        <div className="section-heading">
-          <div className="section-title">
+      <div className="file-explorer-open">
+        <div className="file-explorer-heading">
+          <div className="file-explorer-title">
             <span className="chevron fa-solid fa-chevron-down"></span>PORTFOLIO
           </div>
-          <ul className="sectionButtons">
-            <li><button>+</button></li>
-            <li><button>-</button></li>
-            <li><button>0</button></li>
+          <ul className="file-explorer-buttons">
+            <li><button className='file-explorer-button'>+</button></li>
+            <li><button className='file-explorer-button'>-</button></li>
+            <li><button className='file-explorer-button'>0</button></li>
           </ul>
         </div>
-        <div className="section-content">
+        <div className="file-explorer-content">
         </div>
       </div>
     </div>
