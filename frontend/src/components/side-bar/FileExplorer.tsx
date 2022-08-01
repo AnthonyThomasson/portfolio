c9a7aac6638124e3f43e3ea8ffb8ab19c228617d
Anthony Thomasson <athomasson.93@gmail.com>
2022-08-01 13:49:21 -0600

    - got the dropdown thing going

--- frontend/src/components/side-bar/FileExplorer.tsx
+++ frontend/src/components/side-bar/FileExplorer.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import './../../styles/FileExplorer.css';
 
 function FileExplorer() {
+
+  const [isOpen,setIsOpen] = useState(true)
+
   return (
     <div className="file-explorer">
       <div className="file-explorer-open">
-        <div className="file-explorer-heading">
+        <div className="file-explorer-heading" onClick={() => setIsOpen(!isOpen)}>
           <div className="file-explorer-title">
-            <span className="chevron fa-solid fa-chevron-down"></span>PORTFOLIO
+            <span className={`chevron fa-solid ${isOpen === true ? 'fa-chevron-down' : 'fa-chevron-right'}`}></span>PORTFOLIO
           </div>
           <ul className="file-explorer-buttons">
             <li><button className='file-explorer-button'>+</button></li>
@@ -14,7 +18,8 @@ function FileExplorer() {
             <li><button className='file-explorer-button'>0</button></li>
           </ul>
         </div>
-        <div className="file-explorer-content">
+        <div className={`file-explorer-content ${isOpen === true ? 'file-explorer-content-active' : ''}`}>
+          Stuff Here
         </div>
       </div>
     </div>
---
+++ frontend/src/components/side-bar/Folder.tsx
@@ -0,0 +1,10 @@
+
+function Folder() {
+  return (
+    <div className="folder">
+      <span className='folder-icon'></span><span className='folder-name'></span>
+    </div>   
+  );
+}
+
+export default Folder;
