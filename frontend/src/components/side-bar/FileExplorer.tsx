029573c2678202a20d4be5ad645fef6f600e1a47
Anthony Thomasson <athomasson.93@gmail.com>
2022-08-01 12:22:27 -0600

    - working on file exporer heading

--- frontend/src/components/side-bar/FileExplorer.tsx
+++ frontend/src/components/side-bar/FileExplorer.tsx
@@ -4,7 +4,14 @@ function FileExplorer() {
     <div className="file-explorer">
       <div className="section section-open">
         <div className="section-heading">
-          <span></span>PORTFOLIO
+          <div className="section-title">
+            <span className="chevron fa-solid fa-chevron-down"></span>PORTFOLIO
+          </div>
+          <ul className="sectionButtons">
+            <li><button>+</button></li>
+            <li><button>-</button></li>
+            <li><button>0</button></li>
+          </ul>
         </div>
         <div className="section-content">
         </div>
