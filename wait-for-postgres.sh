81a0b94ef0e945ba4ad6e1912edf25d99dc2ab42
Anthony Thomasson <athomasson.93@gmail.com>
2022-08-24 22:52:39 -0600

    that should do it

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -2,11 +2,9 @@
 # wait-for-postgres.sh
 
 until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
-  >&2 echo "PGPASSWORD=$DB_PASSWORD psql -h '$DB_HOST' -U '$DB_USER' -d '$DB_NAME'"
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
   
 >&2 echo "Postgres is up - executing command"
->&2 echo "Trying to run $@"
 exec "$@"
