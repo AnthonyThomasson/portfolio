a4adaded7bcdbeb9593056e683a7042abfd78a8f
Anthony Thomasson <athomasson.93@gmail.com>
2022-08-24 22:34:52 -0600

    - log environment variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 const path = require('path')
 const fs = require('fs')
 
@@ -8,40 +8,43 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
 
-const { Client } = require('pg')
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ?? 5432,
-})
-client.connect(function(err:any) {
-  if (err) throw err;
-  console.log("Connected to Postgres!");
-});
-
-app.get('/api/files', async (req: Request, res: Response) => {
-  let response = await client.query('SELECT * FROM system_nodes')
-  res.send(response.rows);
-});
-
-app.get('/api/files/:id',async (req: Request, res: Response) => {
-  let response = await client.query("SELECT * FROM system_nodes WHERE id = $1", [req.params.id])
-  res.send(response.rows[0]);
-});
-
-app.get('/api/technologies', async (req: Request, res: Response) => {
-  let response = await client.query('SELECT * FROM technologies')
-  res.send(response.rows);
-});
+console.log(process.env)
 
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/*', function(req,res) {
-		res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// const { Client } = require('pg')
+// const client = new Client({
+//   user: process.env.DB_USER,
+//   host: process.env.DATABASE_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT ?? 5432,
+// })
+// client.connect(function(err:any) {
+//   if (err) throw err;
+//   console.log("Connected to Postgres!");
+// });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+// app.get('/api/files', async (req: Request, res: Response) => {
+//   let response = await client.query('SELECT * FROM system_nodes')
+//   res.send(response.rows);
+// });
+
+// app.get('/api/files/:id',async (req: Request, res: Response) => {
+//   let response = await client.query("SELECT * FROM system_nodes WHERE id = $1", [req.params.id])
+//   res.send(response.rows[0]);
+// });
+
+// app.get('/api/technologies', async (req: Request, res: Response) => {
+//   let response = await client.query('SELECT * FROM technologies')
+//   res.send(response.rows);
+// });
+
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/*', function(req,res) {
+// 		res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+// });
