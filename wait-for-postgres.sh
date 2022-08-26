92e221f5c3b7b03c3998657ac85521bbdfe8988e
Anthony Thomasson <athomasson.93@gmail.com>
2022-08-26 17:40:48 -0600

    using data url for connection

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # wait-for-postgres.sh
 
-until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+until psql $DATABASE_URL -c '\q'; do
+  
+  
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
