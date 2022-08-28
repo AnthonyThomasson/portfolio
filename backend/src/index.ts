74f340cc0f257d5ca2a3ab35ba758475e4de1df7
Anthony Thomasson <athomasson.93@gmail.com>
2022-08-27 19:13:55 -0600

    Added Prettier and eslint to Server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,55 +1,59 @@
-import dotenv from "dotenv";
-import express, { Express, Request, Response } from "express";
-const { Client } = require("pg");
-const path = require("path");
-const fs = require("fs");
+import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express'
+const { Client } = require('pg')
+const path = require('path')
+const fs = require('fs')
 
-dotenv.config();
-const app: Express = express();
-const port = process.env.PORT ?? 3000;
+dotenv.config()
+const app: Express = express()
+const port = process.env.PORT ?? 3000
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.ENVIRONMENT === "development"
-      ? false
-      : {
-          rejectUnauthorized: false,
-        },
-});
+    connectionString: process.env.DATABASE_URL,
+    ssl:
+        process.env.ENVIRONMENT === 'development'
+            ? false
+            : {
+                  rejectUnauthorized: false,
+              },
+})
 
 client.connect(function (err: any) {
-  if (err) throw err;
-  console.log("Connected to Postgres!");
-});
+    if (err) throw err
+    console.log('Connected to Postgres!')
+})
 
-app.get("/api/files", async (req: Request, res: Response) => {
-  let response = await client.query("SELECT * FROM system_nodes");
-  res.send(response.rows);
-});
+app.get('/api/files', async (req: Request, res: Response) => {
+    let response = await client.query('SELECT * FROM system_nodes')
+    res.send(response.rows)
+})
 
-app.get("/api/files/:id", async (req: Request, res: Response) => {
-  let response = await client.query(
-    "SELECT * FROM system_nodes WHERE id = $1",
-    [req.params.id]
-  );
-  res.send(response.rows[0]);
-});
+app.get('/api/files/:id', async (req: Request, res: Response) => {
+    let response = await client.query(
+        'SELECT * FROM system_nodes WHERE id = $1',
+        [req.params.id]
+    )
+    res.send(response.rows[0])
+})
 
-app.get("/api/technologies", async (req: Request, res: Response) => {
-  let response = await client.query("SELECT * FROM technologies");
-  res.send(response.rows);
-});
+app.get('/api/technologies', async (req: Request, res: Response) => {
+    let response = await client.query('SELECT * FROM technologies')
+    res.send(response.rows)
+})
 
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(port, () => {
-  if (process.env.ENVIRONMENT === "development") {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  } else {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-  }
-});
+    if (process.env.ENVIRONMENT === 'development') {
+        console.log(
+            `⚡️[server]: Server is running at http://localhost:${port}`
+        )
+    } else {
+        console.log(
+            `⚡️[server]: Server is running at https://localhost:${port}`
+        )
+    }
+})
