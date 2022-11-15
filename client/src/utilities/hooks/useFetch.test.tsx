0f641ef4aaee9ab9a6b9c87e77985afc47d3f59b
Anthony Thomasson <athomasson.93@gmail.com>
2022-11-14 22:07:17 -0700

    Created a functional fetch utility

--- client/src/utilities/hooks/useFetch.test.tsx
+++ client/src/utilities/hooks/useFetch.test.tsx
@@ -1,8 +1,10 @@
-import { renderHook, waitFor } from '@testing-library/react'
+import { sequenceT } from 'fp-ts/lib/Apply'
+import * as E from 'fp-ts/lib/Either'
+import { pipe } from 'fp-ts/lib/function'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { describe, expect, it } from 'vitest'
-import { useFetch } from './useFetch'
+import { fetchEither } from './useFetch'
 
 const server = setupServer(
     rest.get<ITestResponse>('/api/fetch-example', async (req, res, ctx) => {
@@ -23,22 +25,38 @@ interface ITestResponse {
     name: string
 }
 
-describe.concurrent('useFetch hook', () => {
-    it('should return data', async () => {
-        const { result } = renderHook(() =>
-            useFetch<ITestResponse | null>('/api/fetch-example', null)
-        )
-
-        const [dataBefore, loadingBefore] = result.current
-        expect(loadingBefore).toBe(true)
-        expect(dataBefore).toBe(null)
-        await waitFor(() => {
-            const [dataAfter, loadingAfter] = result.current
-            expect(loadingAfter).toBe(false)
-            expect(dataAfter).toEqual({
-                id: 1,
-                name: 'Example Name',
-            })
-        })
-    })
+describe.concurrent('fetchEither', () => {
+    it('should return data', async () =>
+        pipe(
+            await fetchEither<ITestResponse>('/api/fetch-example')(),
+            E.map((response) =>
+                expect(response).toEqual({
+                    id: 1,
+                    name: 'Example Name',
+                })
+            )
+        ))
+    it('should return an error', async () =>
+        sequenceT(E.Apply)(
+            E.of(
+                server.resetHandlers(
+                    rest.get<ITestResponse>(
+                        '/api/fetch-example',
+                        async (req, res, ctx) => {
+                            return await res(ctx.status(500))
+                        }
+                    )
+                )
+            ),
+            E.of(
+                pipe(
+                    await fetchEither<ITestResponse>('/api/fetch-example')(),
+                    E.mapLeft((error) =>
+                        expect(error.message).toEqual(
+                            'AxiosError: Request failed with status code 500'
+                        )
+                    )
+                )
+            )
+        ))
 })
--- client/src/utilities/hooks/useFetch.tsx
+++ client/src/utilities/hooks/useFetch.tsx
@@ -1,4 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
+import { pipe } from 'fp-ts/lib/function'
+import TE from 'fp-ts/lib/TaskEither'
 import { useEffect, useState } from 'react'
 
 export const useFetch = <T,>(
@@ -24,3 +26,14 @@ export const useFetch = <T,>(
     }, [url])
     return [data, loading, error]
 }
+
+export const fetchEither = <ResponseData,>(
+    url: string
+): TE.TaskEither<Error, ResponseData> =>
+    pipe(
+        TE.tryCatch(
+            async () => await axios.get(url),
+            (reason) => new Error(String(reason))
+        ),
+        TE.map((response) => response.data)
+    )
